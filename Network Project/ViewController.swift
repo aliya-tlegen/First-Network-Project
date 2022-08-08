8d1bbf4e00e3e84e0d3ef8c39ff86d9fc3be274d
aliya <tlegen.aliya@gmail.com>
2022-08-08 21:23:13 +0600

    Initial Commit

---
+++ Network Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Network Project
+//
+//  Created by Алия Тлеген on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
